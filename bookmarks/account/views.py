94ee2723fa5a3cc7e5ea1a73c570dc1d12c381c1
Emil <nero19970610@gmail.com>
2023-07-27 11:48:06 +0500

    Add register page

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,9 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from account.forms import LoginForm
+from account.forms import LoginForm, UserRegistrationForm
 
 
 @login_required
 def dashboard(request):
     return render(request, "account/dashboard.html", {"section": "dashboard"})
+
+
+def register(requset):
+    if requset.method == "POST":
+        user_form = UserRegistrationForm(requset.POST)
+        if user_form.is_valid():
+            # Создаем нового пользователя, но пока не сохраняем в базе данных
+            new_user = user_form.save(commit=False)
+            # Задаем пользователю зашифрованный пароль
+            new_user.set_password(user_form.cleaned_data["password"])
+            # Сохраняем пользователя в базе данных
+            new_user.save()
+            return render(requset, "account/register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(requset, "account/register.html", {"user_form": user_form})
